cae0252a5211e4ec4ad843ea8c1100959a4a336d
Shivam Srivastava <9876srivastavashiavm@gmail.com>
2023-09-17 01:01:31 +0530

    Payment related errors corrected

--- index.js
+++ index.js
@@ -81,6 +81,8 @@ server.use('/auth', authRouters.router);
 server.use('/users', isAuth(), usersRouter.router);
 server.use('/cart', isAuth(), cartRouter.router);
 server.use('/orders', isAuth(), ordersRouter.router)
+// this line we add to make react router work in case of other routes doesnt match
+server.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 
 passport.use(
   'local',
@@ -152,7 +154,7 @@ passport.deserializeUser(function (user, cb) {
 const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 server.post("/create-payment-intent", async (req, res) => {
-  const { totalAmount } = req.body;
+  const { totalAmount, orderId } = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -162,6 +164,9 @@ server.post("/create-payment-intent", async (req, res) => {
     automatic_payment_methods: {
       enabled: true,
     },
+    metadata:{
+      orderId
+    }
   });
 
   res.send({
